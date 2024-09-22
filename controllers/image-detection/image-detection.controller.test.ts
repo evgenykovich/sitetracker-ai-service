7b806fe38448bc18051a807500951463f38a5b4d
evgenykovich <jenya.issakovic@gmail.com>
2024-09-22 15:29:28 +0300

    adding tests

--- controllers/image-detection/image-detection.controller.test.ts
+++ controllers/image-detection/image-detection.controller.test.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express'
 import { getImageDetection } from './image-detection.controller'
-import { AISelectorEnum, convertToBase64 } from '../../utils'
-import { detect } from '../../services'
+import { AIAction, AISelectorEnum, convertToBase64 } from '../../utils'
+import {
+  awsRekognitionDetectImage,
+  claudeDetectImage,
+  detect,
+  geminiDetectImage,
+} from '../../services'
 
 jest.mock('../../services')
 jest.mock('../../utils', () => ({
@@ -87,5 +92,102 @@ describe('getImageDetection', () => {
     expect(detect).toHaveBeenCalledWith('base64Image', ['item1', 'item2'])
     expect(mockStatus).toHaveBeenCalledWith(200)
     expect(mockJson).toHaveBeenCalledWith({ detectedItems: ['detectedItem'] })
+  })
+  it('should handle string input for items', async () => {
+    mockRequest.fields = {
+      items: ['item1, item2'],
+      aiToUse: [AISelectorEnum.OPEN_AI],
+    }
+    mockRequest.files = {
+      image: [{ filepath: 'test/path' } as any],
+    }
+    ;(convertToBase64 as jest.Mock).mockResolvedValue('base64Image')
+    ;(detect as jest.Mock).mockResolvedValue(['detectedItem'])
+
+    await getImageDetection(mockRequest as Request, mockResponse as Response)
+
+    expect(detect).toHaveBeenCalledWith('base64Image', ['item1', 'item2'])
+    expect(mockStatus).toHaveBeenCalledWith(200)
+    expect(mockJson).toHaveBeenCalledWith({ detectedItems: ['detectedItem'] })
+  })
+  it('should use Gemini AI for detection', async () => {
+    mockRequest.fields = {
+      items: ['item1', 'item2'],
+      aiToUse: [AISelectorEnum.GEMINI],
+    }
+    mockRequest.files = {
+      image: [{ filepath: 'test/path' } as any],
+    }
+    ;(convertToBase64 as jest.Mock).mockResolvedValue('base64Image')
+    ;(geminiDetectImage as jest.Mock).mockResolvedValue(['detectedItem'])
+
+    await getImageDetection(mockRequest as Request, mockResponse as Response)
+
+    expect(geminiDetectImage).toHaveBeenCalledWith(
+      'base64Image',
+      AIAction.DETECT,
+      ['item1', 'item2']
+    )
+    expect(mockStatus).toHaveBeenCalledWith(200)
+    expect(mockJson).toHaveBeenCalledWith({ detectedItems: ['detectedItem'] })
+  })
+  it('should use Claude AI for detection', async () => {
+    mockRequest.fields = {
+      items: ['item1', 'item2'],
+      aiToUse: [AISelectorEnum.CLAUDE],
+    }
+    mockRequest.files = {
+      image: [{ filepath: 'test/path' } as any],
+    }
+    ;(convertToBase64 as jest.Mock).mockResolvedValue('base64Image')
+    ;(claudeDetectImage as jest.Mock).mockResolvedValue(['detectedItem'])
+
+    await getImageDetection(mockRequest as Request, mockResponse as Response)
+
+    expect(claudeDetectImage).toHaveBeenCalledWith(
+      'base64Image',
+      AIAction.DETECT,
+      ['item1', 'item2']
+    )
+    expect(mockStatus).toHaveBeenCalledWith(200)
+    expect(mockJson).toHaveBeenCalledWith({ detectedItems: ['detectedItem'] })
+  })
+
+  it('should use AWS Rekognition for detection', async () => {
+    mockRequest.fields = {
+      items: ['item1', 'item2'],
+      aiToUse: [AISelectorEnum.AWS_REKOGNITION],
+    }
+    mockRequest.files = {
+      image: [{ filepath: 'test/path' } as any],
+    }
+    ;(convertToBase64 as jest.Mock).mockResolvedValue('base64Image')
+    ;(awsRekognitionDetectImage as jest.Mock).mockResolvedValue([
+      'detectedItem',
+    ])
+
+    await getImageDetection(mockRequest as Request, mockResponse as Response)
+
+    expect(awsRekognitionDetectImage).toHaveBeenCalledWith('base64Image', [
+      'item1',
+      'item2',
+    ])
+    expect(mockStatus).toHaveBeenCalledWith(200)
+    expect(mockJson).toHaveBeenCalledWith({ detectedItems: ['detectedItem'] })
+  })
+  it('should return 400 for invalid AI selection', async () => {
+    mockRequest.fields = {
+      items: ['item1', 'item2'],
+      aiToUse: ['INVALID_AI'] as any,
+    }
+    mockRequest.files = {
+      image: [{ filepath: 'test/path' } as any],
+    }
+    ;(convertToBase64 as jest.Mock).mockResolvedValue('base64Image')
+
+    await getImageDetection(mockRequest as Request, mockResponse as Response)
+
+    expect(mockStatus).toHaveBeenCalledWith(400)
+    expect(mockJson).toHaveBeenCalledWith({ error: 'Invalid AI selection' })
   })
 })
